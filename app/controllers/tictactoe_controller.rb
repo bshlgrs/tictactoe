bbb841cc4eb1a23ecae4df0f621b6031b0eff2a7
Buck Shlegeris <bshlegeris@paypal.com>
2015-09-29 13:52:17 -0700

    wow wow wow

--- app/controllers/tictactoe_controller.rb
+++ app/controllers/tictactoe_controller.rb
@@ -2,6 +2,18 @@ class TictactoeController < ApplicationController
   def move
     sent_board = Board.from_string(params["board"])
 
-    render text: sent_board.to_wave_string
+    if sent_board
+      if sent_board.is_valid?
+        if sent_board.plausibly_the_turn_of_o?
+          render text: sent_board.to_wave_string
+        else
+          render text: "error: it is not plausibly the turn of o.", status: 400
+        end
+      else
+        render text: "error: this board has an inbalanced number of heads and tails", status: 400
+      end
+    else
+      render text: "error: this was not a valid board", status: 400
+    end
   end
 end
--- lib/Board.rb
+++ lib/Board.rb
@@ -6,6 +6,10 @@ class Board
   end
 
   def self.from_string(string)
+    # todo: error handling
+
+    return nil unless string.length == 9
+
     grid = [0, 1, 2].map do |row_index|
       [0, 1, 2].map do |col_index|
         case string[row_index * 3 + col_index]
@@ -13,8 +17,10 @@ class Board
           :cross
         when "o"
           :naught
-        when "+"
+        when " "
           nil
+        else
+          return nil
         end
       end
     end
@@ -41,6 +47,15 @@ class Board
 
   def count_mark(mark)
     flattened_grid.count(mark)
+  end
+
+  def is_valid?
+    (count_mark(:naught) - count_mark(:cross)).abs < 2
+  end
+
+  def plausibly_the_turn_of_o?
+    # what do I do if the board is invalid?
+    count_mark(:naught) == count_mark(:cross) || count_mark(:naught) == count_mark(:cross) - 1
   end
 end
 
